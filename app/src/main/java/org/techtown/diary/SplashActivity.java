fb9d159ad5432a6f1f5640058347872ef528db28
khjin9026 <khjin9026>
2020-10-07 16:54:57 +0900

    핸들러 주석 추가

--- app/src/main/java/org/techtown/diary/SplashActivity.java
+++ app/src/main/java/org/techtown/diary/SplashActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
-    Handler handler = new Handler();
+    Handler handler = new Handler(); //메서드 내에서 스레드를 실행할 때 사용
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
